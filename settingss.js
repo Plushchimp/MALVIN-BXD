386de517ccfdfa7c73b7c6666bd144813c73d0ba
Plushchimp <88704487+Plushchimp@users.noreply.github.com>
2025-08-31 22:34:35 -0500

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,10 +1,10 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~g3NC3KRL#d1PYyj-bTobrt_PwSkD17mut2o6f9_yFcBDmPwbb1TI",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "18767077170", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
-  TIMEZONE: "Africa/Harare" //put your country timeZone....leave blank if u don't know.
+  TIMEZONE: "Jamaica" //put your country timeZone....leave blank if u don't know.
 };
